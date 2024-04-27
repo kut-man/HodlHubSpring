1ca98352867bc5d2471e0a160017302a3f8c6a97
Kutman <kutman.stel@gmail.com>
2024-04-27 18:50:39 +0200

    enabling CORS

--- src/main/java/com/example/hodlhub/config/SecurityConfig.java
+++ src/main/java/com/example/hodlhub/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.hodlhub.security.CustomAuthenticationSuccessHandler;
 import com.example.hodlhub.security.CustomLogoutSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,7 +19,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/auth/register").permitAll().anyRequest().authenticated())
                 .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
                         .loginPage("/login")
